d93fa2d19671e83f19b73a4bdd3c397005437646
ohdair <ohdiar@gmail.com>
2023-11-14 12:16:21 +0900

    docs: 프로젝트 생성
    - Main Storyboard 삭제

---
+++ Sudoku/Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by 박재우 on 11/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
