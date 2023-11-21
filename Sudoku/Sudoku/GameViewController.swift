939121047f3ef346ec8c0e84d87a79ba84b1064c
ohdair <ohdiar@gmail.com>
2023-11-21 17:54:12 +0900

    feat: AbilityButton 추가
    - undo, erase, memo 기능을 enum을 통해 생성
    - toggleMemo 메서드로 on/off UI 변경

--- Sudoku/Sudoku/GameViewController.swift
+++ Sudoku/Sudoku/GameViewController.swift
@@ -13,11 +13,27 @@ class GameViewController: UIViewController {
 
     private var isTimerRun: Bool = true
 
+    private var abilityStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
         setUI()
+        setLayout()
+
+        for ability in AbilityButton.Ability.allCases {
+            let abilityButton = AbilityButton(of: ability)
+            abilityStackView.addArrangedSubview(abilityButton)
+
+            if ability == .memo {
+                abilityButton.addTarget(self, action: #selector(tappedMemoButton), for: .touchDown)
+            }
+        }
     }
 
     private func setUI() {
@@ -29,6 +45,19 @@ class GameViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = pauseBarButtonItem
     }
 
+    private func setLayout() {
+        view.addSubview(abilityStackView)
+
+        abilityStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            abilityStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            abilityStackView.heightAnchor.constraint(equalToConstant: 60),
+            abilityStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            abilityStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
     @objc private func tappedBackBarButton(_ sender: UIBarButtonItem) {
         self.navigationController?.popViewController(animated: true)
     }
@@ -36,5 +65,9 @@ class GameViewController: UIViewController {
     @objc private func tappedPauseBarButton(_ sender: UIBarButtonItem) {
         isTimerRun.toggle()
         pauseBarButtonItem.image = isTimerRun ? UIImage(systemName: "pause.circle") : UIImage(systemName: "play.circle")
+    }
+
+    @objc private func tappedMemoButton(_ sender: AbilityButton) {
+        sender.toggle()
     }
 }
---
+++ Sudoku/Sudoku/View/AbilityButton.swift
@@ -0,0 +1,137 @@
+//
+//  AbilityButton.swift
+//  Sudoku
+//
+//  Created by 박재우 on 11/21/23.
+//
+
+import UIKit
+
+class AbilityButton: UIButton {
+    private let abilityImageLabel = {
+        let imageViewLabel = UILabel()
+        imageViewLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        imageViewLabel.textColor = .brightMainColor2
+        imageViewLabel.textAlignment = .center
+        return imageViewLabel
+    }()
+
+    private let abilityLabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .brightMainColor2
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let memoToggleLabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.text = "off"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.backgroundColor = .brightMainColor2
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        label.layer.borderWidth = 2
+        label.layer.borderColor = UIColor.systemBackground.cgColor
+        return label
+    }()
+
+    private let type: Ability
+    private(set) var isOnMemo = false
+
+    init(of type: Ability) {
+        self.type = type
+
+        super.init(frame: .zero)
+
+        abilityImageLabel.attributedText = mutableAttributedString(in: type.image)
+        abilityLabel.text = type.description
+
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setLayout() {
+        addSubview(abilityImageLabel)
+        addSubview(abilityLabel)
+
+        abilityImageLabel.translatesAutoresizingMaskIntoConstraints = false
+        abilityLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            abilityImageLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            abilityImageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            abilityImageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+
+            abilityLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            abilityLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            abilityLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+
+        if type == .memo {
+            addSubview(memoToggleLabel)
+            memoToggleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                memoToggleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+                memoToggleLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.7),
+                memoToggleLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.3),
+                memoToggleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 5),
+            ])
+        }
+    }
+
+    private func mutableAttributedString(in image: UIImage) -> NSMutableAttributedString {
+        let mutableAttributedString = NSMutableAttributedString()
+        let imageAttachment = NSTextAttachment(image: image)
+        let imageToString = NSAttributedString(attachment: imageAttachment)
+        mutableAttributedString.append(imageToString)
+        return mutableAttributedString
+    }
+
+    func toggleMemo() {
+        isOnMemo.toggle()
+        if isOnMemo {
+            memoToggleLabel.text = "on"
+            memoToggleLabel.backgroundColor = .mainColor
+        } else {
+            memoToggleLabel.text = "off"
+            memoToggleLabel.backgroundColor = .brightMainColor2
+        }
+    }
+}
+
+extension AbilityButton {
+    enum Ability: CaseIterable {
+        case undo
+        case erase
+        case memo
+
+        var description: String {
+            switch self {
+            case .undo:
+                return "undo"
+            case .erase:
+                return "erase"
+            case .memo:
+                return "memo"
+            }
+        }
+
+        var image: UIImage {
+            switch self {
+            case .undo:
+                return UIImage(systemName: "arrow.uturn.backward")!
+            case .erase:
+                return UIImage(systemName: "eraser")!
+            case .memo:
+                return UIImage(systemName: "pencil.line")!
+            }
+        }
+    }
+}
